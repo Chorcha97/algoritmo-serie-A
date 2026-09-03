15fe281594722006471d2b7f9704a380336a1feb
Cristina Ferro <cristina.ferro@corvallis.it>
2026-09-03 16:13:11 +0200

    [DEV] add game statistics endpoint

--- backend/routers/sofascore.py
+++ backend/routers/sofascore.py
@@ -53,19 +53,113 @@ async def get_team_of_the_period(
 
 @router.get("/serie-a/results")
 async def get_results(request: Request, round: int = 1):
+    return await get_helper_season(request, endpoint=f"events/round/{round}")
+
+# get della classifica finale
+@router.get("/serie-a/standings/total")
+async def get_standings_total(request: Request):
+    return await get_helper_season(request, endpoint=f"standings/total")
+
+@router.get("/serie-a/average_positions")
+async def get_average_positions(
+    request: Request,
+    game_id: int
+):
+    return await get_helper_event(request, endpoint="average-positions", game_id=game_id)
+
+@router.get("/serie-a/lineups")
+async def get_lineups(
+    request: Request,
+    game_id: int
+):
+    return await get_helper_event(request, endpoint="lineups", game_id=game_id)
+
+@router.get("/serie-a/best-players/summary")
+async def get_best_players(
+        request: Request,
+        game_id: int
+):
+    return await get_helper_event(request, endpoint="best-players/summary", game_id=game_id)
+
+@router.get("/serie-a/incidents")
+async def get_incidents(
+    request: Request,
+    game_id: int
+):
+    return await get_helper_event(request, endpoint="incidents", game_id=game_id)
+
+@router.get("/serie-a/shotmap")
+async def get_shotmap(
+    request: Request,
+    game_id: int
+):
+    return await get_helper_event(request, endpoint="shotmap", game_id=game_id)
+
+@router.get("/serie-a/team-streaks")
+async def get_team_streaks(
+    request: Request,
+    game_id: int
+):
+    return await get_helper_event(request, endpoint="team-streaks", game_id=game_id)
+
+@router.get("/serie-a/statistics")
+async def get_statistics(
+    request: Request,
+    game_id: int
+):
+    return await get_helper_event(request, endpoint="statistics", game_id=game_id)
+
+@router.get("/serie-a/h2h")
+async def get_h2h(
+    request: Request,
+    game_id: int
+):
+    return await get_helper_event(request, endpoint="h2h", game_id=game_id)
+
+@router.get("/serie-a/goal-distributions")
+async def get_goal_distributions(request: Request, team_id: int):
+    return await get_helper_season(request, endpoint=f"goal-distributions", pre_endpoint=f"/team/{team_id}")
+
+@router.get("/serie-a/statistics/overall")
+async def get_statistics_overall(request: Request, team_id: int):
+    return await get_helper_season(request, endpoint=f"statistics/overall", pre_endpoint=f"/team/{team_id}")
+
+@router.get("/serie-a/featured-players")
+async def get_featured_players(request: Request, team_id: int):
+    return await get_helper_team(request, endpoint=f"featured-players", team_id=team_id)
+
+@router.get("/serie-a/team-statistics/seasons")
+async def get_team_statistics(request: Request, team_id: int):
+     return await get_helper_team(request, endpoint=f"team-statistics/seasons", team_id=team_id)
+
+async def get_helper_team(request: Request, endpoint: str, team_id: int, params: dict | None = None):
     client: AsyncSession = request.app.state.http_client
+
     response = await api_get_sofascore(
         client,
-        f"/unique-tournament/{T_ID}/season/{S_ID}/events/round/{round}",
+        f"/team/{team_id}/{endpoint}",
+        params=params,
     )
+
     return response.json()
 
-async def get_helper_season(request: Request,  endpoint: str, params: dict | None = None):
+async def get_helper_event(request: Request, endpoint: str, game_id: int, params: dict | None = None):
     client: AsyncSession = request.app.state.http_client
 
     response = await api_get_sofascore(
         client,
-        f"/unique-tournament/{T_ID}/season/{S_ID}/{endpoint}",
+        f"/event/{game_id}/{endpoint}",
+        params=params,
+    )
+
+    return response.json()
+
+async def get_helper_season(request: Request, endpoint: str, pre_endpoint: str = "", params: dict | None = None):
+    client: AsyncSession = request.app.state.http_client
+
+    response = await api_get_sofascore(
+        client,
+        f"{pre_endpoint}/unique-tournament/{T_ID}/season/{S_ID}/{endpoint}",
         params=params,
     )
 
